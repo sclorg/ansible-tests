c167131ddff03ca6d55cacf64669b1b9fc3ab8b3
Petr "Stone" Hracek <phracek@redhat.com>
2022-03-10 18:31:28 +0100

    Fix get pod status
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- files/check_pod_running.sh
+++ files/check_pod_running.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 POD_NAME="$1"
-
 test -z "${POD_NAME}" && echo "pod_name variable is not defined in yml" && exit 1
 
-POD_RUNNING=$(oc get pods --no-headers -o custom-columns=NAME:.metadata.name,STATUS:.status.phase \
-            | grep -v "${POD_NAME}-1-build" | grep -v "${POD_NAME}-1-deploy" \
-            | grep "${POD_NAME}" | cut -d' ' -f 1)
-if [ x"${POD_RUNNING}" != "x" ]; then
-    echo "${POD_RUNNING}"
-    exit 0
-fi
+POD_RESULT_NAME=""
+for i in {1..10}; do
+  POD_RUNNING=$(oc get pods --no-headers -o custom-columns=NAME:.metadata.name,STATUS:.status.phase \
+              | grep -v "${POD_NAME}-1-build" | grep -v "${POD_NAME}-1-deploy" \
+              | grep "${POD_NAME}" | sed 's/  */ /g' | cut -d ' ' -f 1)
+  POD_STATUS=$(oc get pods --no-headers -o custom-columns=NAME:.metadata.name,STATUS:.status.phase \
+              | grep -v "${POD_NAME}-1-build" | grep -v "${POD_NAME}-1-deploy" \
+              | grep "${POD_NAME}" | sed 's/  */ /g' | cut -d ' ' -f 2)
+  if [ x"${POD_STATUS}" == "xRunning" ]; then
+    if [ x"${POD_RESULT_NAME}" != x"${POD_RUNNING}" ]; then
+      POD_RESULT_NAME="${POD_RUNNING}"
+    else
+      echo "$POD_RESULT_NAME"
+      exit 0
+    fi
+  fi
+  if [ x"${POD_STATUS}" == "xFailed" ]; then
+    exit 1
+  fi
+  sleep 60
+done
 exit 1
